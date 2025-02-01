a926647a7e64536822652489443fb9e550b433d9
kanochi <unimrq@163.com>
2025-02-01 21:28:32 +0800

    v3.5 新增图片预览功能

--- static/js/preview-modal.js
+++ static/js/preview-modal.js
@@ -1,20 +1,13 @@
 // script.js
 let currentImageIndex = 0;
 let scale = 1;
-let startDistance = 0;
-let startScale = 1;
 // 定义拖动的变量
 let isLongPress = false;
 let startX, startY;
 let offsetX = 0, offsetY = 0;
-let startOffsetX = 0;
-let startOffsetY = 0;
 let img_offsetX = 0, img_offsetY = 0;
-let lastX = 0;
-let lastY = 0;
 let pressTimer;
-let isTouch = false; // 标记是否为触摸事件
-
+let lastTouchTime = 0;
 
 const previewImage = document.getElementById('preview-image');
 const modal = document.getElementById('preview-modal');
@@ -69,30 +62,10 @@ nextBtn.addEventListener('click', () => {
         imageCount.textContent = `${currentImageIndex + 1} / ${links.length}`;
 });
 
-// 两指缩放
-// 计算两点之间的距离
-// function getDistance(touch1, touch2) {
-//     const dx = touch2.pageX - touch1.pageX;
-//     const dy = touch2.pageY - touch1.pageY;
-//     return Math.sqrt(dx * dx + dy * dy);
-// }
-
-// 监听两指放大缩小
-// previewImage.addEventListener('touchstart', (e) => {
-//     if (e.touches.length === 2) {
-//         // 当两指接触时，记录开始时的距离
-//         startDistance = getDistance(e.touches[0], e.touches[1]);
-//         startScale = scale;
-//     }
-// });
-
 previewImage.addEventListener('dblclick', (e) => {
 
     const centerX = window.innerWidth / 2;
     const centerY = window.innerHeight / 2;
-    // console.log("center", centerX, centerY)
-    // startOffsetX *= scale
-    // startOffsetY *= scale
     if (scale < 9) {
         // 向上滚动：放大
         // console.log("offset1", img_offsetX, img_offsetY)
@@ -113,32 +86,6 @@ previewImage.addEventListener('dblclick', (e) => {
     }
 
 });
-
-// previewImage.addEventListener('touchmove', (e) => {
-//     if (e.touches.length === 2) {
-//         const newDistance = getDistance(e.touches[0], e.touches[1]);
-//         const distanceRatio = newDistance / startDistance;
-//         scale = startScale * distanceRatio;
-//
-//         // 限制缩放范围
-//         scale = Math.max(1, Math.min(scale, 3)); // 设定最小缩放为1，最大为3
-//
-//         // 更新图片的transform属性
-//         previewImage.style.transform = `scale(${scale})`;
-//         if (scale > 1.05){
-//             previewImage.style.zIndex = 1000
-//         }else {
-//             previewImage.style.zIndex = 0
-//         }
-//     }
-// });
-
-// previewImage.addEventListener('touchend', (e) => {
-//     if (e.touches.length < 2) {
-//         startDistance = 0;
-//         startScale = scale;
-//     }
-// });
 
 // 监听鼠标滚轮缩放
 previewImage.addEventListener('wheel', (e) => {
@@ -212,20 +159,12 @@ modal.addEventListener('mouseup', () => {
     // 清除定时器
     clearTimeout(pressTimer);
     isLongPress = false;
-    // 更新起始偏移值
-    // startOffsetX = offsetX;
-    // startOffsetY = offsetY;
-    // previewImage.style.cursor = 'grab'; // 恢复为抓取样式
 });
 
 modal.addEventListener('mouseleave', () => {
     // 清除定时器
     clearTimeout(pressTimer);
     isLongPress = false;
-    // 更新起始偏移值
-    // startOffsetX = offsetX;
-    // startOffsetY = offsetY;
-    // previewImage.style.cursor = 'grab'; // 恢复为抓取样式
 });
 
 
@@ -244,11 +183,11 @@ previewImage.addEventListener('touchmove', function(e) {
       const moveY = e.touches[0].clientY - startY;
 
       // 更新偏移量
-      offsetX = moveX;
-      offsetY = moveY;
+      img_offsetX += moveX;
+      img_offsetY += moveY;
 
       // 更新图片位置
-      previewImage.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+      previewImage.style.transform = `scale(${scale}) translate(${img_offsetX}px, ${img_offsetY}px)`;
 
       // 防止默认的触摸滚动行为
       e.preventDefault();
@@ -263,9 +202,6 @@ previewImage.addEventListener('touchend', function(e) {
         // 双击事件发生，时间间隔小于300ms视为双击
         const centerX = window.innerWidth / 2;
         const centerY = window.innerHeight / 2;
-        // console.log("center", centerX, centerY)
-        // startOffsetX *= scale
-        // startOffsetY *= scale
         if (scale < 9) {
             // 向上滚动：放大
             // console.log("offset1", img_offsetX, img_offsetY)
@@ -285,6 +221,5 @@ previewImage.addEventListener('touchend', function(e) {
             previewImage.style.transform = `scale(${scale}) translate(${img_offsetX}px, ${img_offsetY}px)`;
         }
     }
-
     lastTouchTime = currentTime; // 更新上次触摸时间
 });
